0d45a70803e5e90f2de07c92039721846e3c9c0b
El Diablo <yohannesrahul3@gmail.com>
2025-12-11 15:00:13 +0700

    commit Test Frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PageFont from './components/PageFront';
+
+function App() {
+  return (
+    <div>
+      <PageFont />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PageFront.jsx
@@ -0,0 +1,23 @@
+export default function PageFront() {
+  return (
+    <section className="py-10 bg-white w-full">
+      <div className="max-w-5xl mx-auto text-center px-4">
+        <h2 className="text-xl font-semibold text-gray-800 mb-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+
+          <div className="bg-gray-200 h-40 rounded-lg flex justify-center items-center"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
